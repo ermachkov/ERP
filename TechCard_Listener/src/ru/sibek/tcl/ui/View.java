5a5b6d860f7df03bf730dd8b6bdfd93acd751c35
root <root@fc17x64-usb.beefymiracle>
2012-10-03 17:20:33 +0700

    3.10.12

---
+++ TechCard_Listener/src/ru/sibek/tcl/ui/View.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sibek.tcl.ui;
+
+/**
+ *
+ * @author toor
+ */
+public class View {
+    
+}
