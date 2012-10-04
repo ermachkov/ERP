deca54707b13c01a787c53c526b58cac8af5a99f
root <root@fc17x64-usb.beefymiracle>
2012-10-04 17:23:41 +0700

    4.10.12

---
+++ TechCardGUI/src/ru/sibek/techcard/ui/Specification.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sibek.techcard.ui;
+
+/**
+ *
+ * @author toor
+ */
+public class Specification {
+    
+}
